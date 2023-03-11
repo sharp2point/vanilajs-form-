4ea624ea0a949d4b9bd4cd5736ae8993cb8526ba
Evgeny Blinov <sharppoint@yandex.ru>
2023-03-11 23:34:13 +0300

    add startup init Database

--- productsDB.js
+++ productsDB.js
@@ -1,6 +1,8 @@
+import Product from "./product.js";
+
 class ProductDB{
     constructor(){
-        this._products = [];
+        this._products = [new Product("xiaomi","125"),new Product("iphon 14","1050"),new Product("samsung","275"),];
     }
     add = (product)=>{
         this._products.push(product)
